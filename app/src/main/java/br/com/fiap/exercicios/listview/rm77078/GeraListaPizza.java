5beeff2191ce6006109a8959cac1af6e341988f9
Ricardo Santos Filho <rickardosf@live.com>
2017-03-31 10:28:18 -0300

    Atualizado o nome da pizza de frano com catupiry

--- app/src/main/java/br/com/fiap/exercicios/listview/rm77078/GeraListaPizza.java
+++ app/src/main/java/br/com/fiap/exercicios/listview/rm77078/GeraListaPizza.java
@@ -13,7 +13,7 @@ public class GeraListaPizza {
 
         pizzas.add(new Pizzas("Pizza de Mussarela","Ingredientes", R.drawable.mussarela, "R$ 22,50", "9"));
         pizzas.add(new Pizzas("Pizza de Calabresa", "Ingredientes", R.drawable.calabresa, "R$ 21,50", "8"));
-        pizzas.add(new Pizzas("Pizza de Catupiry", "Ingredientes", R.drawable.catupiry, "R$ 42,50", "9"));
+        pizzas.add(new Pizzas("Pizza de Frango com Catupiry", "Ingredientes", R.drawable.catupiry, "R$ 42,50", "9"));
         pizzas.add(new Pizzas("Pizza Portuguesa", "Ingredientes", R.drawable.portuguesa, "R$ 33,50", "7"));
         pizzas.add(new Pizzas("Pizza de Milho", "Ingredientes", R.drawable.milho, "R$ 25,50", "6"));
         pizzas.add(new Pizzas("Pizza de Bacon", "Ingredientes", R.drawable.bacon, "R$ 35,50", "7"));
